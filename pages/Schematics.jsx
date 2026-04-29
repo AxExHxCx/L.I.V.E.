6732d35f74eb6cfef10d0e9329db1113b0355d66
AxExHxCx <blackzombie138@gmail.com>
2026-04-29 03:08:06 -0400

    Create Schematics component for displaying diagrams

---
+++ pages/Schematics.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Schematics() {
+  return (
+    <div style={{ padding: '40px' }}>
+      <h1>Schematics Page</h1>
+      <p>Technical drawings and diagrams will be displayed here.</p>
+    </div>
+  );
+}
