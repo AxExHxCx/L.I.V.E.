f96c70f0a166ff3a4f592d45026b0937c17d7aa6
AxExHxCx <blackzombie138@gmail.com>
2026-04-29 02:55:09 -0400

    Add UserNotRegisteredError component

---
+++ components/UserNotRegisteredError.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function UserNotRegisteredError() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Access Denied</h1>
+      <p>You are not registered to use this application.</p>
+    </div>
+  );
+}
