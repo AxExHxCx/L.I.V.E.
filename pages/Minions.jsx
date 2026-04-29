5dcb0036ee29cfc45942d617526153ce664c8ab6
AxExHxCx <blackzombie138@gmail.com>
2026-04-29 03:11:04 -0400

    Create Minions component with basic layout

---
+++ pages/Minions.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Minions() {
+  return (
+    <div style={{ padding: '40px' }}>
+      <h1>Minions Page</h1>
+      <p>Your team or character list will appear here.</p>
+    </div>
+  );
+}
