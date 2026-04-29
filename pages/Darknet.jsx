989c82413444034d171b51300e74005bb531aebd
AxExHxCx <blackzombie138@gmail.com>
2026-04-29 03:16:24 -0400

    Add Darknet component for secure connection display

---
+++ pages/Darknet.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Darknet() {
+  return (
+    <div style={{ padding: '40px', backgroundColor: '#000', color: '#ff0000' }}>
+      <h1>Darknet Access</h1>
+      <p>Secure connection established...</p>
+    </div>
+  );
+}
