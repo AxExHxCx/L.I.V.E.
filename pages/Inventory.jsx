59db0675b547b90a87a71a53682e2a0b6413e83a
AxExHxCx <blackzombie138@gmail.com>
2026-04-29 03:01:53 -0400

    Create Inventory component with basic layout

---
+++ pages/Inventory.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Inventory() {
+  return (
+    <div style={{ padding: '40px' }}>
+      <h1>Inventory Page</h1>
+      <p>Your inventory items will appear here.</p>
+    </div>
+  );
+}
