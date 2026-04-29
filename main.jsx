5d4db14ab22f29f16bc8c9d9a423508b0182d96a
AxExHxCx <blackzombie138@gmail.com>
2026-04-29 02:11:08 -0400

    Add Base44 client and use React.StrictMode
    Integrate Base44 SDK and wrap App component in StrictMode.

--- main.jsx
+++ main.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
+import { createClient } from '@base44/sdk'
+
+const base44 = createClient({
+  appId: '69e4274134a0810ed78c9ad6',
+})
+
+globalThis.__B44_DB__ = base44
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
